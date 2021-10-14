5f5356ef71c3048bc47d01372eed4fd79b7f64ee
ulgoon <ulgoon89@gmail.com>
2021-10-14 15:50:04 +0900

    feat: Stack size, clear, print (Complete)

--- resources/datastructure/stack.js
+++ resources/datastructure/stack.js
@@ -30,8 +30,22 @@ class Stack {
         return this.count===0
     } // return true or false
 
-    size(){continue}
-    clear(){}
+    size(){
+        console.log(`Stack size: ${this.count}`)
+        return this.count
+    }
+
+    print(){
+        console.log(`${this.items.toString()}`)
+        return this.items.toString()
+    }
+
+    clear(){
+        this.items = []
+        this.count = 0
+        console.log('Reset Stack complete')
+        return this.items
+    } // initialize
 }
 
 const stack = new Stack()
@@ -46,5 +60,12 @@ stack.pop()
 
 stack.isEmpty()
 stack.peek()
-stack.push('banana')
+stack.push('bike')
 stack.peek()
+
+stack.size()
+stack.print()
+
+stack.clear()
+stack.size()
+stack.print()
